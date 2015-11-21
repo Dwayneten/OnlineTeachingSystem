becb4748d5b7239e2596fa43c83155ed2dfebf65
Dwayneten <151809668@qq.com>
2015-11-21 17:13:41 +0800

    + Add article test page.

---
+++ OnlineTeachingSystem/Controllers/ArticleController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTeachingSystem.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public ActionResult Test()
+        {
+            return View("Test");
+        }
+    }
+}
